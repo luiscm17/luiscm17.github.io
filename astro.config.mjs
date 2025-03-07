42ae414ded58ce4ddf53b93e2e3b42f80883b7b5
luis-cm-ideapad3-win <115508520+luiscm17@users.noreply.github.com>
2025-03-07 02:24:50 -0400

    thirds deploy fix errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://luiscm17.github.io',
-    base: 'luiscm-portolio',
+    base: 'luiscm-portfolio',
     integrations: [mdx(), react(), tailwind({
         applyBaseStyles: false,
     }),
